dc05a935656998a76785c7e225a6de6c44a7c1d8
Olya Wysocky and Pavel Zanchuk <olya@>
2019-04-03 15:12:33 -0700

    ready app

---
+++ src/app/edit-task/edit-task.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
+
+
+@Component({
+  selector: 'app-edit-task',
+  templateUrl: './edit-task.component.html',
+  styleUrls: ['./edit-task.component.css']
+})
+export class EditTaskComponent{
+
+  @Input() childSelectedTask: Task;
+  @Output() clickedDone = new EventEmitter;
+
+  finishedEditingClicked() {
+    this.clickedDone.emit();
+  }
+
+}
